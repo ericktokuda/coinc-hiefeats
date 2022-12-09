400bb22daa3a231093b13227e38c29d670bed017
Eric K. Tokuda <eric.tokuda@maths.ox.ac.uk>
2022-12-09 09:11:26 +0000

    Update main function

--- src/main.py
+++ src/main.py
@@ -135,58 +135,6 @@ def calculate_hierclucoeff(he, hn):
 def calculate_hieconvratio(hd, hnnxt):
     return hd / hnnxt
 
-
-##########################################################
-def graph_from_dfs(dfes, dfvs):
-    """Short description """
-    info(inspect.stack()[0][3] + '()')
-    g = igraph.Graph(n=len(dfvs))
-    g.vs['wid'] = dfvs.wid.values
-    g.vs['title'] = dfvs.title.values
-    g.add_edges(dfes[['srcvid', 'tgtvid']].values)
-    g.simplify(multiple=True, loops=True)
-    return g
-
-##########################################################
-def get_induced_subgraph(snppath, widspath, outdir):
-    """Get the subgraph induced by the vertices with @wids wiki ids"""
-    info(inspect.stack()[0][3] + '()')
-
-    snpfiltpath = pjoin(outdir, 'snap0_es.tsv')
-    widsfiltpath = pjoin(outdir, 'snap0_vs.tsv')
-
-    if isfile(snpfiltpath):
-        dfsnpfilt = pd.read_csv(snpfiltpath, sep='\t')
-        dfwidsfilt = pd.read_csv(widsfiltpath, sep='\t')
-        return dfsnpfilt, dfwidsfilt
-
-    dfsnp = pd.read_csv(snppath, sep='\t')
-    dfwids = pd.read_csv(widspath, sep='\t')
-    dfwids = dfwids.loc[dfwids.ns == 0] # Filter pages from the main ns
-
-    # Filter snapshot by the reference vertices
-    dfsnpfilt = dfsnp.merge(dfwids, how='inner', left_on='src', right_on='id')
-    dfsnpfilt = dfsnpfilt.merge(dfwids, how='inner', left_on='tgt', right_on='id')
-
-    # Get titles of the vertices
-    uwids = sorted(np.unique(dfsnpfilt[['src', 'tgt']].values.flatten()))
-    dfwidsfilt = pd.DataFrame(uwids, columns=['wid']).merge(
-        dfwids, how='inner', left_on='wid', right_on='id')
-    dfwidsfilt = dfwidsfilt.drop_duplicates(keep='first')
-    dfwidsfilt.to_csv(widsfiltpath, sep='\t', index=False,
-                    columns=['wid', 'title'],)
-
-    n = len(uwids)
-    wid2vid = {wid:vid for vid, wid in enumerate(uwids)}
-
-    # Convert wiki-id to vertex id
-    dfsnpfilt = dfsnpfilt[['src', 'tgt']].copy()
-    dfsnpfilt['srcvid'] = dfsnpfilt.src.map(wid2vid)
-    dfsnpfilt['tgtvid'] = dfsnpfilt.tgt.map(wid2vid)
-    dfsnpfilt.to_csv(snpfiltpath, sep='\t', index=False,
-                    columns=['src', 'tgt', 'srcvid', 'tgtvid'])
-    return dfsnpfilt, dfwidsfilt
-
 ##########################################################
 def extract_hirarchical_feats(adj, v, h):
     """Extract hierarchical features"""
@@ -221,97 +169,7 @@ def vattributes2edges(g, attribs, aggreg='sum'):
     return g
 
 ##########################################################
-def plot_motifs(gorig, edges, coincpath, outdir):
-    info(inspect.stack()[0][3] + '()')
-
-    outpath = pjoin(outdir, 'coinc.pdf')
-    coinc = np.loadtxt(coincpath, delimiter=',', dtype=float)
-    coinc = np.power(coinc, 3)
-    gcoinc = igraph.Graph.Weighted_Adjacency(coinc, mode='undirected')
-    gcoinc.delete_edges(gcoinc.es.select(weight_lt=.4))
-
-    # comm = gcoinc.community_multilevel(weights='weight')
-    ncomms = 5
-    comm = gcoinc.components(mode='weak')
-    szs = comm.sizes()
-    largestcommids = np.flip(np.argsort(szs))[:ncomms]
-    membs = np.array(comm.membership)
-    m = gcoinc.vcount()
-
-    palette = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']
-    vcolours = np.array(['#d9d9d9'] * m)
-
-    ##########################################################
-    # Making the colormap
-    from matplotlib.colors import LinearSegmentedColormap
-    cm = LinearSegmentedColormap.from_list(
-        'new',
-        ['#d9d9d9'] + palette[:ncomms] ,
-        N = 8)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(np.random.random((3,3)), interpolation ='nearest',
-                    origin ='lower', cmap = cm)
-    # ax.set_title("bin: % s" % all_bin)
-    fig.colorbar(im, ax=ax)
-    plt.savefig(pjoin(outdir, 'colorbar.pdf'))
-
-    ##########################################################
-    for i in range(ncomms):
-        vcolours[np.where(membs == largestcommids[i])[0]] = palette[i]
-
-    outpath = pjoin(outdir, 'coinc.pdf')
-    igraph.plot(gcoinc, outpath, edge_width=np.abs(gcoinc.es['weight']),
-                bbox=(1200, 1200),
-                vertex_size=5, vertex_color=vcolours.tolist())
-
-    ##########################################################
-    g = gorig.copy()
-    eids = g.get_eids(edges)
-    todel = set(range(g.ecount())).difference(eids)
-
-    g.es['comm'] = -1
-    for i in range(ncomms):
-        aux = np.array(edges)[np.where(membs == largestcommids[i])[0]]
-        g.es[g.get_eids(aux)]['comm'] = i
-    aux = np.array(edges)[np.where(membs == largestcommids[0])[0]]
-    g.es[g.get_eids(aux)]['comm'] = 0
-
-    g.delete_edges(todel)
-    degs = g.degree()
-    g.delete_vertices(np.where(np.array(degs) == 0)[0])
-
-    membs = np.array(g.es['comm'])
-    m = g.ecount()
-    ecolours = np.array(['#d9d9d9'] * m)
-
-    # fh = open('/tmp/titles.lst', 'w')
-    # membslbls = []
-    for i in range(ncomms):
-        ecolours[np.where(membs == i)[0]] = palette[i]
-        vv = np.unique(np.array([ [x.source, x.target] for x in np.array(g.es)[np.where(membs == i)[0]] ]).flatten())
-        # membslbls.append(np.array(g.vs['title'])[vv])
-        # fh.write(str(i) + '\n')
-        # fh.write('\n'.join(membslbls[i]))
-        # fh.write('\n\n')
-    # fh.close()
-
-    outpath = pjoin(outdir, 'orig.png')
-    igraph.plot(g, outpath, bbox=(1200, 1200),
-                # vertex_label=g.vs['title'],
-                vertex_color='black',
-                vertex_size=50,
-                edge_color=ecolours.tolist())
-
-    outpath = pjoin(outdir, 'orig.pdf')
-    igraph.plot(g, outpath, bbox=(1200, 1200),
-                # vertex_label=g.vs['title'],
-                vertex_color='black',
-                vertex_size=50,
-                edge_color=ecolours.tolist())
-
-##########################################################
-def plot_motifs2(gorig, coincpath, outdir):
+def plot_motifs(gorig, coincpath, outdir):
     info(inspect.stack()[0][3] + '()')
 
     outpath = pjoin(outdir, 'coinc.pdf')
@@ -421,15 +279,29 @@ def plot_motifs2(gorig, coincpath, outdir):
 def main(outdir):
     info(inspect.stack()[0][3] + '()')
 
-    random.seed(0); np.random.seed(0)
-    # n = 101
-    p = 0.05
-    h = 2
+    argsconcat = []
+    for i, f in enumerate(fs):
+        if not is_historical_dump(f): continue
+        argsconcat.append((i, pjoin(dumpdir, f), skipids, debug, outdir))
+
+    argsconcat = argsconcat[startid:]
+    parallelize(process_dump_protected, nprocs, argsconcat)
+
+    # Generate graph
+    # Extract features
+    # Calculate coincidence similarity
+    # Create coinc graph
+    # Determine components
+    # Calculate statistics in each group
+    # Plot distributions for each
 
     # Generate the graph
     # g = igraph.Graph.Erdos_Renyi(n, p, directed=False, loops=False)
     # breakpoint()
-
+    random.seed(0); np.random.seed(0)
+    # n = 101
+    p = 0.05
+    h = 2
     g = igraph.Graph.GRG(200, radius=0.2, torus=False)
     g.to_undirected()
 
@@ -447,23 +319,21 @@ def main(outdir):
     efeats = np.array([g.es[l] for l in labels]).T
     # coinc, coincpath = get_coincidx_graph(efeats, .5, True, outdir)
     coinc, coincpath = get_coincidx_graph(vfeats, .5, True, outdir)
-    # edges = [[e.source, e.target] for e in g.es]
-    # plot_motifs(g, edges, coincpath, outdir)
-    plot_motifs2(g, coincpath, outdir)
-
+    plot_motifs(g, coincpath, outdir)
 
 ##########################################################
 if __name__ == "__main__":
     info(datetime.date.today())
     t0 = time.time()
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--nruns', default=1, type=int,
+                        help='Number of runs for each experiment')
+    parser.add_argument('--nprocs', default=1, type=int, help='Number of procs')
     parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
     args = parser.parse_args()
 
     os.makedirs(args.outdir, exist_ok=True)
     readmepath = create_readme(sys.argv, args.outdir)
-
-    main(args.outdir)
-
+    main(args.nprocs, args.outdir)
     info('Elapsed time:{:.02f}s'.format(time.time()-t0))
     info('Output generated in {}'.format(args.outdir))
